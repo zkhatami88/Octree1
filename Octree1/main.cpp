2dcc922e37c2849d886bde560821b4c630df324f
Zahra Khatami <zkhatami@Zahras-MacBook-Pro.local>
2015-03-20 08:20:51 -0500

    Initial Commit

---
+++ Octree1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Octree1
+//
+//  Created by Zahra Khatami on 3/20/15.
+//  Copyright (c) 2015 Zahra Khatami. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
